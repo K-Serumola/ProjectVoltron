660d33e5980f9cedb644df72f9ca8b39524ecd73
K-Serumola <cse24-008@thuto.bac.ac.bw>
2025-06-05 18:09:46 +0200

    vbn

---
+++ src/main/java/com/vangard/projectvoltron/ProjectVoltronApplication.java
@@ -0,0 +1,13 @@
+package com.vangard.projectvoltron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectVoltronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectVoltronApplication.class, args);
+    }
+
+}
