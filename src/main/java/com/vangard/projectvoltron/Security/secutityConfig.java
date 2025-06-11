77fc3e52ab6a626e34092e410e0a4e3c7bd0144b
K-Serumola <cse24-008@thuto.bac.ac.bw>
2025-06-11 19:07:13 +0200

    im dieing slowly

--- src/main/java/com/vangard/projectvoltron/Security/secutityConfig.java
+++ src/main/java/com/vangard/projectvoltron/Security/secutityConfig.java
@@ -1,10 +1,17 @@
 package com.vangard.projectvoltron.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.vangard.projectvoltron.Model.MyAppUserService;
 
 import lombok.AllArgsConstructor;
 
@@ -13,9 +20,26 @@ import lombok.AllArgsConstructor;
 @EnableWebSecurity
 public class secutityConfig {
 
+    @Autowired
+    private final MyAppUserService appUserService;
+    
+    
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return appUserService;
+    }
+    
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(appUserService);
+        return provider;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
         return httpSecurity
+            .csrf(AbstractHttpConfigurer::disable)
             .formLogin(httpForm ->{
                 httpForm
                     .loginPage("/login").permitAll();
@@ -23,7 +47,7 @@ public class secutityConfig {
             })
 
             .authorizeHttpRequests(registry ->{
-                registry.requestMatchers("/req/signup" , "/css/**" , "/js/**").permitAll();
+                registry.requestMatchers("/req/signup" , "/css/**" , "/js/**" ,"/templates/**").permitAll();
                 registry.anyRequest().authenticated();
             })
 
