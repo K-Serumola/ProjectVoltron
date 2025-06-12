67da9f24b28b51b9b4fbcfda85658274f819ebb4
K-Serumola <cse24-008@thuto.bac.ac.bw>
2025-06-12 20:14:40 +0200

    wha

--- src/main/java/com/vangard/projectvoltron/Security/secutityConfig.java
+++ src/main/java/com/vangard/projectvoltron/Security/secutityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.vangard.projectvoltron.Model.MyAppUserService;
@@ -34,6 +36,11 @@ public class secutityConfig {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(appUserService);
         return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
