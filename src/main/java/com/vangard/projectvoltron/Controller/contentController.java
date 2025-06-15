051f643225ac670345beebee60bb4a4d195eb6ff
JadenWBAC <cse24-085@thuto.bac.ac.bw>
2025-06-15 09:59:39 +0200

    make the things work

--- src/main/java/com/vangard/projectvoltron/Controller/contentController.java
+++ src/main/java/com/vangard/projectvoltron/Controller/contentController.java
@@ -1,67 +1,67 @@
 package com.vangard.projectvoltron.Controller;
-
+ 
 import java.security.Principal;
 import java.util.Optional;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+ 
 import com.vangard.projectvoltron.Model.MyAppUser;
 import com.vangard.projectvoltron.Model.MyAppUserRepository;
-
+ 
 @Controller
 public class contentController {
-
+ 
     @GetMapping("/login")
     public String login(){
         return "login";
     }
-
+ 
     @GetMapping("/req/signup")
     public String signup(Model model) {
         model.addAttribute("user", new MyAppUser()); // Important!
         return "signup";
     }
-    
+   
     @GetMapping("/index")
     public String home(){
         return "index";
     }
-
+ 
     @GetMapping("/quiz")
     public String quiz(){
         return "quiz";
     }
-
+ 
     @GetMapping("/learn")
     public String learnpage(){
         return "learn";
     }
-
+ 
     @GetMapping("/mindbank")
     public String mindbankPage(){
         return "mindbank";
     }
-
+ 
     @GetMapping("/nubi")
     public String nubi(){
         return "nubi";
     }
-
+ 
     @Autowired
     private MyAppUserRepository myAppUserRepository;
-
+ 
     @GetMapping("/profile")
     public String profilepage(Model model, Principal principal){
     if (principal == null) {
         return "redirect:/login";
     }
-
+ 
     String username = principal.getName();
     Optional<MyAppUser> userOptional = myAppUserRepository.findByUsername(username);
-
+ 
     if (userOptional.isPresent()) {
         MyAppUser user = userOptional.get();
         model.addAttribute("user", user);
@@ -70,14 +70,34 @@ public class contentController {
         return "redirect:/login";
     }
     }
-
+ 
     @GetMapping("/Apptutorial")
     public String apptutorialpage(){
         return "Apptutorial";
     }
-
+ 
     @GetMapping("/save-to-win")
     public String savetowin(){
         return "save-to-win";
+    }
+ 
+    @GetMapping("/money-match")
+    public String moneymatch(){
+        return "money-match";
+    }
+ 
+    @GetMapping("/scam-or-legit")
+        public String scammorlegit(){
+        return "scam-spotter";
+    }
+ 
+    @GetMapping("/budget-buddy")
+    public String bb(){
+        return "budget-buddy";
+    }
+
+    @GetMapping("/spend smart")
+    public String spendSmart(){
+        return "spend-smart";
     }
 }
